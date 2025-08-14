c851dc39602eed0f511e936a50140660d69e30f1
dev-bb38 <skipperbb38@outlook.com>
2025-08-15 00:51:12 +1000

    update build

--- public/build/assets/Home-bC0MNmYU.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:public/build/assets/Home-bC0MNmYU.js
-import{c as o,o as a,a as s,t as r}from"./app-BYSHimyr.js";/* empty css            */const n={class:"p-6"},c={__name:"Home",props:{message:String},setup(t){return(l,e)=>(a(),o("div",n,[e[0]||(e[0]=s("h1",{class:"text-2xl font-bold"},"Home Page",-1)),s("p",null,r(t.message),1)]))}};export{c as default};
-========
-import{c as o,o as a,a as s,t as r}from"./app-DpHJp1cN.js";/* empty css            */const n={class:"p-6"},c={__name:"Home",props:{message:String},setup(t){return(l,e)=>(a(),o("div",n,[e[0]||(e[0]=s("h1",{class:"text-2xl font-bold"},"Home Page",-1)),s("p",null,r(t.message),1)]))}};export{c as default};
->>>>>>>> 9a8d6cab5faf870d12ae90c5225b14e8948f1add:public/build/assets/Home-B9ozCJg1.js
