1f2844d9bee6f5261d4aa70c88d973a7c798dc1e
dev-bb38 <skipperbb38@outlook.com>
2025-08-21 22:00:39 +1000

    meta upgrade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title inertia></title>
+    <title inertia>Laravel + Vue App</title>
+    <meta name="description" content="Default description about my Laravel + Vue app.">
 
     @vite('resources/js/app.js')
     @inertiaHead
