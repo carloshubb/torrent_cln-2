3caaf7dbfb3c97c8730f83fb22358ca9b3f77d63
dev-bb38 <skipperbb38@outlook.com>
2025-08-22 10:26:09 -0400

    upgrade app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,36 +4,54 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title inertia></title>
-    <link rel="canonical" href="https://www.1331x.com">
-    <title> Free Movies TV Series Music Games Software Anime torrent 1331x.com 1337x</title>
-
-
-    <meta name="description" content="Default description about my Laravel + Vue app.">
-
+    <link rel="canonical" href="https://1331x.com">
+<title> Free Movies TV Series Music Games Software Anime torrent 1331x.com 1337x</title>
+<meta name="keywords" content="1337x, torrents, torrent site, 1337x proxy, movies, 1337, 1337x, 1331x.com, 1337x unblocked, thepiratebay, piratebay proxy, tpb, top torrent site, games, tv series,free streaming, watch online,">
+<meta name="description" content="1331x and 1337x top torrent site, latest movies, games, tv series, music, Anime, torrent, software, utorrent, bittorrent, qbittorrent, streaming">
+<h1 class="hidden"> 1331x Torrent no 1 site, Main Domain 1331x.com .</h1>
+<h2 class="hidden"> Best Torrent Site For Movies, HD, 4k, 8k, Music, Applications, Television, Games, Anime, Documentaries, Windows,1331x.com..</h2>
+<meta property="og:title" content="Free Movies TV Series Music Games Software Anime torrent 1331x.com 1337x" />
+<meta property="og:type" content="1331x and 1337x top torrent site, latest movies, games, tv series, music, Anime, torrent, software, utorrent, bittorrent, qbittorrent, streaming, watch online top torrent siteabsolutely for free" />
+<meta property="og:url" content="https://1331x.com" />
+<meta property="og:image" content="https://1331x.com/1331x.svg" />
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://1331x.com">
+<meta property="og:type" content="website">
+<meta property="og:title" content="Free Movies TV Series Music Games Software Anime torrent 1331x.com 1337x">
+<meta property="og:description" content="1331x and 1337x top torrent site, latest movies, games, tv series, music, Anime, torrent, software, utorrent, bittorrent, qbittorrent, streaming, watch online top torrent siteabsolutely for free">
+<meta property="og:image" content="https://1331x.com/1331x.svg">
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta property="twitter:domain" content="1331x.com">
+<meta property="twitter:url" content="https://1331x.com">
+<meta name="twitter:title" content="Free Movies TV Series Music Games Software Anime torrent 1331x.com 1337x">
+<meta name="twitter:description" content="1331x and 1337x top torrent site, latest movies, games, tv series, music, Anime, torrent, software, utorrent, bittorrent, qbittorrent, streaming, watch online top torrent siteabsolutely for free">
+<meta name="twitter:image" content="https://1331x.com/1331x.svg">
+<h3 class="hidden"><a rel="nofollow" href="https://1331x.com/cat/Movies/1" title="Movies"><span class="icon"><i class="flaticon-movies"></i></span>Movies </a></h3>
+<h3 class="hidden"><a rel="nofollow" href="https://1331x.com/cat/TV/1" title="Television"><span class="icon"><i class="flaticon-tv"></i></span>Television </a></h3>
+<h3 class="hidden"><a rel="nofollow" href="https://1331x.com/cat/Games/1" title="Games"><span class="icon"><i class="flaticon-games"></i></span>Games </a></h3>
+<h3 class="hidden"><a rel="nofollow" href="https://1331x.com/cat/Music/1" title="Musics"><span class="icon"><i class="flaticon-music"></i></span>Music </a></h3>
+<h3 class="hidden"><a rel="nofollow" href="https://1331x.com/cat/Apps/1" title="Applications"><span class="icon"><i class="flaticon-apps"></i></span>Applications </a></h3>
+<h3 class="hidden"><a rel="nofollow" href="https://1331x.com/cat/Anime/1" title="Anime"><span class="icon"><i class="flaticon-ninja-portrait"></i></span>Anime .</a></h3>
+<div itemscope itemtype="https://schema.org/Torrent">
+<p hidden>Description: 1331x and 1337x top torrent site, latest movies, games, tv series, music, Anime, torrent, software, utorrent, bittorrent, qbittorrent, streaming, watch online top torrent.</p> 
+</div>
     @vite('resources/js/app.js')
     @inertiaHead
 </head>
 <body class="font-sans antialiased">
       @inertia
 </body>
- <!-- Histats.com  START  (aync)-->
-<script type="text/javascript">
-var _Hasync= _Hasync|| [];
+   <!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,4969467,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
-var hs = document.createElement('script'); 
-hs.type = 'text/javascript'; 
-hs.async = true;
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();
-</script>
-<noscript>
-    <a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4969467&101" alt="counter statistics" border="0"></a>
-</noscript>
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4969467&101" alt="free webpage counters" border="0"></a></noscript>
 <!-- Histats.com  END  -->
 </html>
